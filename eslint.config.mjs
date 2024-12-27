2e4151e634f54c44021da3f926c714ec88544455
reswaraa <reswara.ad@gmail.com>
2024-12-27 16:41:27 +0700

    feat: add homepage placeholder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,35 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      ".next/*",
+      "*.css",
+      "esm/*",
+      "public/*",
+      "tests/*",
+      "scripts/*",
+      "*.config.js",
+      "coverage",
+      ".next",
+      "build",
+      "node_modules",
+      "dist",
+    ],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ),
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "off", // Adjust rule to allow require if necessary
+      "@typescript-eslint/no-unused-vars": ["warn", {argsIgnorePattern: "^_"}],
+      "prettier/prettier": ["error"],
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import {Button} from "./_components/ui/button";
+
 export default function Home() {
-  return <div className="">Homepage</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <h1 className="text-3xl font-bold text-gray-900 sm:text-4xl md:text-5xl">
+          GTD XXVII Polog Project
+        </h1>
+        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg">
+          Choose between Escape Room or Case File
+        </p>
+        <Button asChild className="mt-5">
+          <Link href="/register" className="">
+            Register Now
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
