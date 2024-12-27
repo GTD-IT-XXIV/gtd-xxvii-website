e2247e78dff9dde1a5d819bc238f90c1d7fb2a43
reswaraa <reswara.ad@gmail.com>
2024-12-27 17:36:50 +0700

    feat: add navbar

---
+++ src/app/_components/Container.tsx
@@ -0,0 +1,5 @@
+const Container = ({children}: {children: React.ReactNode}) => {
+  return <div className="max-w-[1920px] w-full mx-auto px-4 py-2">{children}</div>;
+};
+
+export default Container;
---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Container from "./Container";
+import Image from "next/image";
+import logoGTDBlack from "@/assets/images/logo-gtd-black-transparent.png";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 border border-b-primary/10">
+      <Container>
+        <div className="flex items-center gap-3">
+          <Link href="/">
+            <Image
+              src={logoGTDBlack}
+              alt="Logo PINTU Get Together Day"
+              className="h-8 w-12 object-cover"
+            />
+          </Link>
+          <Link href="/">Home</Link>
+          <Link href="/register">Register</Link>
+          <Link href="/about">About</Link>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
