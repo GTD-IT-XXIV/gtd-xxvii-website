d8b2d1cf398bbd670e3105ad47e3d978f3e73680
reswaraa <reswara.ad@gmail.com>
2025-01-17 10:16:06 +0800

    refactor: fix formatting and styling

--- src/app/about/_components/background/page.tsx
+++ src/app/about/_components/background/page.tsx
@@ -1,16 +1,22 @@
 export default function Background() {
-    return (
-        <div className="m-4">
-            <h1 className="text-4xl text-center">Polog Project</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <h2 className="text-2xl">Goals</h2>
-            <ul>
-                <li>Goal 1</li>
-                <li>Goal 2</li>
-                <li>Goal 3</li>
-                <li>Goal 4</li>
-                <li>Goal 5</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="m-4">
+      <h1 className="text-4xl text-center">Polog Project</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h2 className="text-2xl">Goals</h2>
+      <ul>
+        <li>Goal 1</li>
+        <li>Goal 2</li>
+        <li>Goal 3</li>
+        <li>Goal 4</li>
+        <li>Goal 5</li>
+      </ul>
+    </div>
+  );
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,13 @@
-import Faq from "./_components/faq/page"
-import Background from "./_components/background/page"
-import Contact from "./_components/contact/page"
+import Faq from "./_components/faq/page";
+import Background from "./_components/background/page";
+import Contact from "./_components/contact/page";
 
 export default function About() {
   return (
-    <div className="p-6 m-auto w-[60%]"> 
-      <Background/>
-      <Faq/>
-      <Contact/>
+    <div className="p-6 m-auto w-[60%]">
+      <Background />
+      <Faq />
+      <Contact />
     </div>
-  )
+  );
 }
