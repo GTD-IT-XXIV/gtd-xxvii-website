a55d14a2cb3847e05a0cc03e09e3a2593448d3e1
reswaraa <reswara.ad@gmail.com>
2025-02-09 21:12:11 +0800

    fix(lenis): type declaration

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,7 +2,7 @@
 
 import HeroSection from "@/app/_components/HeroSection";
 import EventCard from "@/app/_components/EventCard";
-import {ReactLenis} from "lenis/dist/lenis-react";
+import {ReactLenis} from "lenis/react";
 
 export default function Home() {
   return (
